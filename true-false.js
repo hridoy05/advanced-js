9be63de132065c694c4b540ebd072a95d923577f
Hridoy Banik <hridoy.banik71@gmail.com>
2020-03-04 21:17:26 +0600

    truthy vs falsy

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//falsy
+//0
+//""
+//undefined
+//null
+//NaN
+///Truthy
+// "0"
+// ' '
+// []
+// {}
+const array = []
+if ([]) {
+    console.log("show true")
+} else {
+    console.log("otherwise false")
+}
