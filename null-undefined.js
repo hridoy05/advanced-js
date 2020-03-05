c88f8862a21d6caf020fe90f959fddbca2b26c8b
Hridoy Banik <hridoy.banik71@gmail.com>
2020-03-05 10:19:30 +0600

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,16 @@
+let moyna;
+
+function add(num1, num2) {
+    console.log(num1 + num2)
+}
+
+function add(num1, num2) {
+    console.log(num1 + num2)
+}
+const result = add(25);
+console.log(result);
+
+const person = { name: "hridoy", id: 1234 };
+console.log(person.gf);
+let funtion = undefined;
+console.log(function);
